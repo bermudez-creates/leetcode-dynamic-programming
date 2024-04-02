ac817711ce02f083417c3200685928c114111b4f
Steezy <bermudezalejandro45@gmail.com>
2024-04-02 12:04:58 -0400

    Longest string - Fail last case

---
+++ README.txt
@@ -0,0 +1,12 @@
+# Learning by doing
+
+By 
+
+# Goal
+
+Understand how to break down Leetcode problems
+
+Become accustomed to looking at data structures and algorithms
+
+Learn pattern recognition to solve problems faster 
+
--- longestSubstring.js
+++ longestSubstring.js
@@ -2,16 +2,34 @@
 // without repeating characters.
 
 const lengthOfSubstring = (s) => {
-  console.log(s);
-  for (let i = 0; i <= s.length; i++) {
-    for (let string of s) {
-      if (string[i] !== undefined) {
-        console.log(`String`, string[i]);
-      }
+  // initial value for longest substring
+  let max = 0;
+
+  // loop through `s` string
+  for (let i = 0; i < s.length; i++) {
+    // Set object lets you store unique values of any type
+    let current = new Set();
+
+    // loop through letters at current index
+    for (let j = i; j < s.length; j++) {
+      current.add(s[j]);
+      // if (!current.has(s[j])) {
+      //   current.add(s[j]);
+      // } else {
+      // console.log(current);
+
+      //   max = Math.max(max, current.size);
+      //   console.log(max);
+      //   return current;
+      // }
     }
+
+    max = Math.max(max, current.size);
   }
+
+  return max;
 };
 
 console.log(lengthOfSubstring('abcabcbb')); // 3 => The answer is "abc", with the length of 3.
-// console.log(lengthOfSubstring('bbbbb')); // 1 => The answer is "b", with the length of 1.
-// console.log(lengthOfSubstring('pwwkew')); // 3 => The answer is "wke", with the length of 3
+console.log(lengthOfSubstring('bbbbb')); // 1 => The answer is "b", with the length of 1.
+console.log(lengthOfSubstring('pwwkew')); // 3 => The answer is "wke", with the length of 3
